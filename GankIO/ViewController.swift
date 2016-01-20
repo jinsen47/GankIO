b7be2744e02125dce2396566444564c1ed3ec50d
Jinsen Sheng <jinsen47@gmail.com>
2016-01-20 17:57:20 +0800

    [Feature] Update network download image

--- GankIO/ViewController.swift
+++ GankIO/ViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var titleNames = ["20160119", "20160118", "20160117", "20160116", "20160115"]
-    var beautyImages = ["pic1", "pic1", "pic1", "pic1", "pic1"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let API = "http://gank.avosapps.com/api/data/%E7%A6%8F%E5%88%A9/10/1"
+    
+    var titleNames = [String]()
+    var beautyImages = [String]()
+    var mockImage = "pic1"
+    
+    var screenWidth: CGFloat = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadData(API)
+        self.screenWidth = UIScreen.mainScreen().bounds.width
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,15 +44,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.titleLabel.text = titleNames[indexPath.row]
         cell.dateLabel.text = titleNames[indexPath.row]
         
-        let originImage = UIImage(named: beautyImages[indexPath.row])
+        let imageHeight = cell.beautyImageView.bounds.height
+        let imageWidth = cell.beautyImageView.bounds.width
+        
+        let originImage = UIImage(data: NSData(contentsOfURL: NSURL(string: beautyImages[indexPath.row])!)!)
+//        print("第\(indexPath.row)张图片的宽是 \(originImage?.size.width), 高是 \(originImage?.size.height)")
+        
         let resizedImage : UIImage
         if originImage?.size.height > originImage?.size.width {
-            resizedImage = resizeImage(originImage!, size: CGSizeMake(320, (originImage?.size.height)! / (originImage?.size.width)! * 320))
+            let resize = CGSizeMake(imageWidth, (originImage?.size.height)! * imageWidth /  (originImage?.size.width)!)
+//            print("缩放大小 \(resize.width) * \(resize.height)")
+            resizedImage = resizeImage(originImage!, size: resize)
         } else {
-            resizedImage = resizeImage(originImage!, size: CGSizeMake((originImage?.size.width)! / (originImage?.size.height)! * 240, 240))
+            let resize = CGSizeMake((originImage?.size.width)! * imageHeight / (originImage?.size.height)!, imageHeight)
+//            print("缩放大小 \(resize.width) * \(resize.height)")
+            resizedImage = resizeImage(originImage!, size: resize)
+            
         }
         
-        let cropImage = cropRectImage(resizedImage)
+        let cropImage = cropRectImage(resizedImage, targetSize: CGSizeMake(screenWidth, imageHeight))
         
         cell.beautyImageView.image = cropImage
         return cell
@@ -53,6 +73,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return true
     }
     
+    func loadData(url : String) ->Void {
+        let nsUrl  = NSURL(string: url)
+        let request = NSURLRequest(URL: nsUrl!)
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {
+            (response, data, error) -> Void in
+            if ((error) != nil) {
+                print("网络故障")
+            } else {
+                if let jsonData: NSDictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
+                    let resultArray = jsonData["results"] as! Array<NSDictionary>
+                    for item in resultArray {
+                        var publishString = item["publishedAt"] as! String
+                        publishString = publishString.substringToIndex(publishString.startIndex.advancedBy(10))
+                        let imageUrl = item["url"] as! String
+                        
+                        self.beautyImages.append(imageUrl)
+                        self.titleNames.append(publishString)
+                    }
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.self.myTableView.reloadData()
+                    })
+                }
+            }
+            })
+    }
+    
     func resizeImage(image: UIImage, size: CGSize) -> UIImage {
         let sourceSize = image.size
         
@@ -61,9 +108,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         var newSize: CGSize
         if widthRatio > heightRatio {
-            newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
+            newSize = CGSizeMake(sourceSize.width * heightRatio, sourceSize.height * heightRatio)
         } else {
-            newSize = CGSizeMake(size.width * widthRatio, size.height * widthRatio)
+            newSize = CGSizeMake(sourceSize.width * widthRatio, sourceSize.height * widthRatio)
         }
         
         let rect = CGRectMake(0, 0, newSize.width, newSize.height)
@@ -75,8 +122,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return newImage
     }
     
-    func cropRectImage(image: UIImage) -> UIImage {
-        let targetSize = CGSizeMake(320, 240)
+    func cropRectImage(image: UIImage, targetSize :CGSize) -> UIImage {
         let sourceSize = image.size
         
         var cropRect: CGRect
@@ -88,7 +134,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let imageRef = CGImageCreateWithImageInRect(image.CGImage, cropRect)
         if let imageTar = imageRef {
-            print("image crop suceess!")
+//            print("image crop suceess!")
             return UIImage(CGImage: imageTar, scale: UIScreen.mainScreen().scale, orientation: image.imageOrientation)
 
         } else {
